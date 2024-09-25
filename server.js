b8cbf8d7d029b8909e609460d3c4c194933bd511
robiulsagor <robiul100.me@gmail.com>
2024-09-25 21:10:24 +0600

    user regi and login function working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js"
 import userRouter from "./routes/userRouter.js"
+import productRouter from "./routes/productRouter.js"
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -13,10 +14,10 @@ app.use(cors())
 
 // api endpoints
 app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
 
 app.get('/', (req,res)=> {
     return res.json({success: true, message: "Api is working!"})
 })
 
-app.listen(PORT, ()=> console.log("Server started")
-)
+app.listen(PORT, ()=> console.log("Server started"))
