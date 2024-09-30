871ef5c7f7a5d32b0b5ee8f8d3f2ca24f3268f87
robiulsagor <robiul100.me@gmail.com>
2024-09-30 22:09:21 +0600

    mongoose server error solving

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-   mongoose.connection.on('connected', () => {
-      console.log("Database connected!");
-
-   })
-   //  await mongoose.connect(`${process.env.MONGODB_URI}/forever-ecommerce`)
-   await mongoose.connect(process.env.MONGODB_URI)
+   try {
+      mongoose.connection.on('connected', () => {
+         console.log("Database connected!");
+      })
+      mongoose.connection.on('error', (error) => {
+         console.log("Database connection error: ", error);
+      })
+      await mongoose.connect(process.env.MONGODB_URI)
+   } catch (error) {
+      console.error("Failed to connect to MongoDB:", error);
+      process.exit(1);  // Optionally exit the process if the connection fails
+   }
 }
 
 export default connectDB;
