4e072ab53bb35b6f057b07a5e33d21bb5f2d56ad
robiulsagor <robiul100.me@gmail.com>
2024-10-01 17:47:17 +0600

    cors

--- server.js
+++ server.js
@@ -18,6 +18,15 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // List allowed methods if needed
     credentials: true  // If you are sending cookies or authorization headers
 }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://frontend-forever-nu.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 app.use(express.json())
 
 app.use('/api/user', userRouter);
