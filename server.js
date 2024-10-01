843743f7bac11b3466504814af8177aa2a918b91
robiulsagor <robiul100.me@gmail.com>
2024-10-01 20:51:35 +0600

    added cors origin

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const app = express()
 const PORT = process.env.PORT || 5000
 connectDB()
 
+const allowedOrigins = ['https://frontend-forever-lac.vercel.app', 'http://localhost:5173'];
+
 app.options('*', cors());  // Allow preflight requests
 
 app.use(cors({
@@ -20,7 +22,10 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://frontend-forever-lac.vercel.app');
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
